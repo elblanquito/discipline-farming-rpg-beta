a1083b5a7c5d4a7cabbd165771a15d27f228beb6
elblanquito <johanblancocata@gmail.com>
2026-07-15 04:04:37 -0500

    colores hsl con degradado

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,38 @@ function App() {
       description:
         "Descripción de la disciplina, podria ser mitivaciones que lo empujan a realizarlo dia tras dia",
       level: 5,
+      r: 5,
       difficulty: "Media",
     },
     {
       name: "Disciplina 2",
       color: "defecto",
+      description:
+        "Descripción de la disciplina, podria ser mitivaciones que lo empujan a realizarlo dia tras dia",
+      level: 5,
+      difficulty: "Media",
+    },
+    {
+      name: "Disciplina 3",
+      color: {
+        hsl: "hsl(236, 80%, 22%)",
+        h: 236,
+        s: 80,
+        l: 22,
+      },
+      description:
+        "Descripción de la disciplina, podria ser mitivaciones que lo empujan a realizarlo dia tras dia",
+      level: 5,
+      difficulty: "Media",
+    },
+    {
+      name: "Disciplina 4",
+      color: {
+        hsl: "hsl(112, 80%, 22%)",
+        h: 112,
+        s: 80,
+        l: 22,
+      },
       description:
         "Descripción de la disciplina, podria ser mitivaciones que lo empujan a realizarlo dia tras dia",
       level: 5,
@@ -28,7 +55,8 @@ function App() {
       <section className="skills-container">
         <SkillCard skill={skills[0]} />
         <SkillCard skill={skills[1]} />
-        <SkillCard skill={skills[1]} />
+        <SkillCard skill={skills[2]} />
+        <SkillCard skill={skills[3]} />
       </section>
     </>
   );
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,14 +1,22 @@
 export const SkillCard = ({ skill }) => {
+  const lightColor = skill.color.hsl;
+  const darkColor = `hsl(${skill.color.h}, ${skill.color.s + 20}%, ${skill.color.l - 10}%)`;
   return (
-    skill &&
-    <article className={`skill skill-${skill.color}`}>
-      <header>
-        <h2>{skill.name}</h2>
-      </header>
-      <p className="skill-description">{skill.description}</p>
-      <p>Nivel: {skill.level}</p>
-      <p>Dias completados: {skill.level}</p>
-      <p>Dificultad: {skill.difficulty}</p>
-    </article>
+    skill && (
+      <article
+        className={`skill skill-${skill.color}`}
+        style={{
+          background: `linear-gradient(0deg, ${darkColor} 0%, ${lightColor} 100%)`,
+        }}
+      >
+        <header>
+          <h2>{skill.name}</h2>
+        </header>
+        <p className="skill-description">{skill.description}</p>
+        <p>Nivel: {skill.level}</p>
+        <p>Dias completados: {skill.level}</p>
+        <p>Dificultad: {skill.difficulty}</p>
+      </article>
+    )
   );
 };
